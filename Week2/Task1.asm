9553ecdc3b050e469fa9a8ffd2d3d7d138d991c8
Aryan <f20210532@pilani.bits-pilani.ac.in>
2023-04-06 03:10:51 +0530

    Task2 Updated

--- Week2/Task1.asm
+++ Week2/Task1.asm
@@ -7,8 +7,8 @@
 .startup
      ; mov ah, 5bh
      ; mov al, 5dh
-     ; N data.txt
-     ; L 0200
+     ; N data.txt (dosbox command)
+     ; L 0200 (dosbox command)
      ; mov [0200 + 5], ah
      ; mov [0200 + 9], al
 .exit
